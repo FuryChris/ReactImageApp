6a98d053339f79a3cfc65618e4f9c161cf352baa
unknown <desmont2222@gmail.com>
2021-05-09 12:04:07 +0200

    Listing images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const ImageList = (props) => {
-  const images = props.images.map((image) => {
-    return <img src={image.urls.regular} />;
+  const images = props.images.map(({ id, description, urls }) => {
+    // const { id, description, urls } = image; other approach if image is as map agrument
+    return <img key={id} alt={description} src={urls.regular} />;
   });
   return <div>{images}</div>;
 };
